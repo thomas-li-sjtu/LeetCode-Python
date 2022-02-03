a5e5b5d868dca5123410fbbc66bacaa1660456b6
thomas-li-sjtu <980226547@qq.com>
2022-02-03 11:01:41 +0800

    重点是贪心思想的证明，done

---
+++ leetcode/editor/cn/[1414]和为 K 的最少斐波那契数字数目.py
@@ -0,0 +1,69 @@
+# 给你数字 k ，请你返回和为 k 的斐波那契数字的最少数目，其中，每个斐波那契数字都可以被使用多次。 
+# 
+#  斐波那契数字定义为： 
+# 
+#  
+#  F1 = 1 
+#  F2 = 1 
+#  Fn = Fn-1 + Fn-2 ， 其中 n > 2 。 
+#  
+# 
+#  数据保证对于给定的 k ，一定能找到可行解。 
+# 
+#  
+# 
+#  示例 1： 
+# 
+#  输入：k = 7
+# 输出：2 
+# 解释：斐波那契数字为：1，1，2，3，5，8，13，……
+# 对于 k = 7 ，我们可以得到 2 + 5 = 7 。 
+# 
+#  示例 2： 
+# 
+#  输入：k = 10
+# 输出：2 
+# 解释：对于 k = 10 ，我们可以得到 2 + 8 = 10 。
+#  
+# 
+#  示例 3： 
+# 
+#  输入：k = 19
+# 输出：3 
+# 解释：对于 k = 19 ，我们可以得到 1 + 5 + 13 = 19 。
+#  
+# 
+#  
+# 
+#  提示： 
+# 
+#  
+#  1 <= k <= 10^9 
+#  
+#  Related Topics 贪心 
+#  👍 74 👎 0
+
+
+# leetcode submit region begin(Prohibit modification and deletion)
+class Solution(object):
+    def findMinFibonacciNumbers(self, k):
+        """
+        :type k: int
+        :rtype: int
+        """
+        # 证明，对于给定的k，需要选择不小于k的最大斐波那契数
+        fibo_list = [1, 1]
+        while True:
+            a = fibo_list[-1] + fibo_list[-2]
+            if a <= k:
+                fibo_list.append(a)
+            else:
+                break
+        ans = 0
+        for i in range(len(fibo_list)-1, -1, -1):
+            if fibo_list[i] <= k:
+                k -= fibo_list[i]
+                ans += 1
+            if k == 0:
+                return ans
+# leetcode submit region end(Prohibit modification and deletion)
