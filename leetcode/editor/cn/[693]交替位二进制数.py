0f194e3533a3ee9c914e6a4691e439e140e94986
thomas-li-sjtu <980226547@qq.com>
2022-03-28 12:46:19 +0800

    done

---
+++ leetcode/editor/cn/[693]交替位二进制数.py
@@ -0,0 +1,50 @@
+"""
+给定一个正整数，检查它的二进制表示是否总是 0、1 交替出现：换句话说，就是二进制表示中相邻两位的数字永不相同。 
+
+ 
+
+ 示例 1： 
+
+ 
+输入：n = 5
+输出：true
+解释：5 的二进制表示是：101
+ 
+
+ 示例 2： 
+
+ 
+输入：n = 7
+输出：false
+解释：7 的二进制表示是：111. 
+
+ 示例 3： 
+
+ 
+输入：n = 11
+输出：false
+解释：11 的二进制表示是：1011. 
+
+ 
+
+ 提示： 
+
+ 
+ 1 <= n <= 2³¹ - 1 
+ 
+ Related Topics 位运算 👍 159 👎 0
+
+"""
+
+
+# leetcode submit region begin(Prohibit modification and deletion)
+class Solution:
+    def hasAlternatingBits(self, n: int) -> bool:
+        string = str(bin(n))[2:]
+        for i, char in enumerate(string):
+            if i % 2 == 0 and char != "1":
+                return False
+            if i % 2 == 1 and char != "0":
+                return False
+        return True
+# leetcode submit region end(Prohibit modification and deletion)
