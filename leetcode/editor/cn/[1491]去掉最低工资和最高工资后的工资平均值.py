70241f491412f7a2b3ffd08e20b7b6d16e285f7c
thomas-li-sjtu <980226547@qq.com>
2022-04-06 12:45:06 +0800

    done

---
+++ leetcode/editor/cn/[1491]去掉最低工资和最高工资后的工资平均值.py
@@ -0,0 +1,62 @@
+"""
+给你一个整数数组 salary ，数组里每个数都是 唯一 的，其中 salary[i] 是第 i 个员工的工资。 
+
+ 请你返回去掉最低工资和最高工资以后，剩下员工工资的平均值。 
+
+ 
+
+ 示例 1： 
+
+ 输入：salary = [4000,3000,1000,2000]
+输出：2500.00000
+解释：最低工资和最高工资分别是 1000 和 4000 。
+去掉最低工资和最高工资以后的平均工资是 (2000+3000)/2= 2500
+ 
+
+ 示例 2： 
+
+ 输入：salary = [1000,2000,3000]
+输出：2000.00000
+解释：最低工资和最高工资分别是 1000 和 3000 。
+去掉最低工资和最高工资以后的平均工资是 (2000)/1= 2000
+ 
+
+ 示例 3： 
+
+ 输入：salary = [6000,5000,4000,3000,2000,1000]
+输出：3500.00000
+ 
+
+ 示例 4： 
+
+ 输入：salary = [8000,9000,2000,3000,6000,1000]
+输出：4750.00000
+ 
+
+ 
+
+ 提示： 
+
+ 
+ 3 <= salary.length <= 100 
+ 10^3 <= salary[i] <= 10^6 
+ salary[i] 是唯一的。 
+ 与真实值误差在 10^-5 以内的结果都将视为正确答案。 
+ 
+ Related Topics 数组 排序 👍 33 👎 0
+
+"""
+from typing import List
+
+
+# leetcode submit region begin(Prohibit modification and deletion)
+class Solution:
+    def average(self, salary: List[int]) -> float:
+        max_num, min_num, sum_salary = -1, 10 ** 6, 0
+        for i in salary:
+            max_num = max(max_num, i)
+            min_num = min(min_num, i)
+            sum_salary += i
+
+        return (sum_salary - max_num - min_num) / (len(salary) - 2)
+# leetcode submit region end(Prohibit modification and deletion)
