c88e5cb23b860fe7d5b3b6d66671f43b50007909
thomas-li-sjtu <980226547@qq.com>
2022-04-09 12:31:29 +0800

    done

---
+++ leetcode/editor/cn/[438]找到字符串中所有字母异位词.py
@@ -0,0 +1,81 @@
+"""
+给定两个字符串 s 和 p，找到 s 中所有 p 的 异位词 的子串，返回这些子串的起始索引。不考虑答案输出的顺序。 
+
+ 异位词 指由相同字母重排列形成的字符串（包括相同的字符串）。 
+
+ 
+
+ 示例 1: 
+
+ 
+输入: s = "cbaebabacd", p = "abc"
+输出: [0,6]
+解释:
+起始索引等于 0 的子串是 "cba", 它是 "abc" 的异位词。
+起始索引等于 6 的子串是 "bac", 它是 "abc" 的异位词。
+ 
+
+ 示例 2: 
+
+ 
+输入: s = "abab", p = "ab"
+输出: [0,1,2]
+解释:
+起始索引等于 0 的子串是 "ab", 它是 "ab" 的异位词。
+起始索引等于 1 的子串是 "ba", 它是 "ab" 的异位词。
+起始索引等于 2 的子串是 "ab", 它是 "ab" 的异位词。
+ 
+
+ 
+
+ 提示: 
+
+ 
+ 1 <= s.length, p.length <= 3 * 10⁴ 
+ s 和 p 仅包含小写字母 
+ 
+ Related Topics 哈希表 字符串 滑动窗口 👍 836 👎 0
+
+"""
+from typing import List
+
+
+# leetcode submit region begin(Prohibit modification and deletion)
+class Solution:
+    def findAnagrams(self, s: str, p: str) -> List[int]:
+        if len(s) < len(p):
+            return []
+        p_dict, tmp_dict = {}, {}
+        res = []
+        for i in p:
+            if p_dict.get(i):
+                p_dict[i] += 1
+            else:
+                p_dict[i] = 1
+        for i in range(len(p)):
+            if tmp_dict.get(s[i]):
+                tmp_dict[s[i]] += 1
+            else:
+                tmp_dict[s[i]] = 1
+
+        def valid():
+            for key, value in p_dict.items():
+                if not tmp_dict.get(key) or tmp_dict[key] != value:
+                    return False
+            return True
+
+        left, right = 0, len(p)-1  # [left, right]
+        while right < len(s):
+            if valid():
+                res.append(left)
+            left += 1
+            right += 1
+            if right < len(s):
+                tmp_dict[s[left-1]] -= 1
+                if tmp_dict.get(s[right]):
+                    tmp_dict[s[right]] += 1
+                else:
+                    tmp_dict[s[right]] = 1
+        return res
+
+# leetcode submit region end(Prohibit modification and deletion)
