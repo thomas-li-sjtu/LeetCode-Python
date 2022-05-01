cfacb1269cfdc4a9ddd8d972e77c4aa628eda1b6
thomas-li-sjtu <980226547@qq.com>
2022-05-01 13:15:28 +0800

    done

---
+++ leetcode/editor/cn/[1305]两棵二叉搜索树中的所有元素.py
@@ -0,0 +1,79 @@
+"""
+给你 root1 和 root2 这两棵二叉搜索树。请你返回一个列表，其中包含 两棵树 中的所有整数并按 升序 排序。. 
+
+ 
+
+ 示例 1：
+
+ 
+
+ 
+输入：root1 = [2,1,4], root2 = [1,0,3]
+输出：[0,1,1,2,3,4]
+ 
+
+ 示例 2： 
+
+ 
+
+ 
+输入：root1 = [1,null,8], root2 = [8,1]
+输出：[1,1,8,8]
+ 
+
+ 
+
+ 提示： 
+
+ 
+ 每棵树的节点数在 [0, 5000] 范围内 
+ -10⁵ <= Node.val <= 10⁵ 
+ 
+ Related Topics 树 深度优先搜索 二叉搜索树 二叉树 排序 👍 114 👎 0
+
+"""
+
+# leetcode submit region begin(Prohibit modification and deletion)
+# Definition for a binary tree node.
+from typing import List
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def getAllElements(self, root1: TreeNode, root2: TreeNode) -> List[int]:
+        def inorder(node: TreeNode, res: List[int]):
+            if node:
+                inorder(node.left, res)
+                res.append(node.val)
+                inorder(node.right, res)
+
+        nums1, nums2 = [], []
+        inorder(root1, nums1)
+        inorder(root2, nums2)
+
+        merged = []
+        p1, n1 = 0, len(nums1)
+        p2, n2 = 0, len(nums2)
+        while True:
+            if p1 == n1:
+                merged.extend(nums2[p2:])
+                break
+            if p2 == n2:
+                merged.extend(nums1[p1:])
+                break
+            if nums1[p1] < nums2[p2]:
+                merged.append(nums1[p1])
+                p1 += 1
+            else:
+                merged.append(nums2[p2])
+                p2 += 1
+        return merged
+
+        
+# leetcode submit region end(Prohibit modification and deletion)
