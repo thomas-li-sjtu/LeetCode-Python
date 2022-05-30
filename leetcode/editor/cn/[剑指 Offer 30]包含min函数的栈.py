0bc00320019199119715375547cf593e101796cf
thomas-li-sjtu <980226547@qq.com>
2022-05-30 13:11:14 +0800

    用一个辅助栈存放最小值，done

---
+++ leetcode/editor/cn/[剑指 Offer 30]包含min函数的栈.py
@@ -0,0 +1,65 @@
+"""
+定义栈的数据结构，请在该类型中实现一个能够得到栈的最小元素的 min 函数在该栈中，调用 min、push 及 pop 的时间复杂度都是 O(1)。 
+
+ 
+
+ 示例: 
+
+ MinStack minStack = new MinStack();
+minStack.push(-2);
+minStack.push(0);
+minStack.push(-3);
+minStack.min();   --> 返回 -3.
+minStack.pop();
+minStack.top();      --> 返回 0.
+minStack.min();   --> 返回 -2.
+ 
+
+ 
+
+ 提示： 
+
+ 
+ 各函数的调用总次数不超过 20000 次 
+ 
+
+ 
+
+ 注意：本题与主站 155 题相同：https://leetcode-cn.com/problems/min-stack/ 
+ Related Topics 栈 设计 👍 347 👎 0
+
+"""
+import math
+
+
+# leetcode submit region begin(Prohibit modification and deletion)
+class MinStack:
+
+    def __init__(self):
+        """
+        initialize your data structure here.
+        """
+        self.min_stack = [math.inf]
+        self.stack = []
+
+    def push(self, x: int) -> None:
+        self.stack.append(x)
+        self.min_stack.append(min(x, self.min_stack[-1]))
+
+    def pop(self) -> None:
+        self.stack.pop()
+        self.min_stack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def min(self) -> int:
+        return self.min_stack[-1]
+
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(x)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.min()
+# leetcode submit region end(Prohibit modification and deletion)
