4ea378b9a6d7c6043549673257fb0a972fdd1ccf
thomas-li-sjtu <980226547@qq.com>
2022-06-03 13:30:33 +0800

    done

---
+++ leetcode/editor/cn/[剑指 Offer 45]把数组排成最小的数.py
@@ -0,0 +1,43 @@
+"""
+输入一个非负整数数组，把数组里所有数字拼接起来排成一个数，打印能拼接出的所有数字中最小的一个。 
+
+ 
+
+ 示例 1: 
+
+ 输入: [10,2]
+输出: "102" 
+
+ 示例 2: 
+
+ 输入: [3,30,34,5,9]
+输出: "3033459" 
+
+ 
+
+ 提示: 
+
+ 
+ 0 < nums.length <= 100 
+ 
+
+ 说明: 
+
+ 
+ 输出结果可能非常大，所以你需要返回一个字符串而不是整数 
+ 拼接起来的数字可能会有前导 0，最后结果不需要去掉前导 0 
+ 
+ Related Topics 贪心 字符串 排序 👍 469 👎 0
+
+"""
+from typing import List
+
+# leetcode submit region begin(Prohibit modification and deletion)
+class Solution:
+    def minNumber(self, nums: List[int]) -> str:
+        for i in range(len(nums)-1):
+            for j in range(i+1, len(nums)):
+                if str(nums[i]) + str(nums[j]) > str(nums[j]) + str(nums[i]):
+                    nums[i], nums[j] = nums[j], nums[i]
+        return "".join([str(i) for i in nums])
+# leetcode submit region end(Prohibit modification and deletion)
