1b171275ce2940cafc2780b5d3e880c5276f3dce
thomas-li-sjtu <980226547@qq.com>
2022-06-11 08:47:01 +0800

    done

---
+++ leetcode/editor/cn/[剑指 Offer 64]求1+2+…+n.py
@@ -0,0 +1,34 @@
+"""
+求 1+2+...+n ，要求不能使用乘除法、for、while、if、else、switch、case等关键字及条件判断语句（A?B:C）。 
+
+ 
+
+ 示例 1： 
+
+ 输入: n = 3
+输出: 6
+ 
+
+ 示例 2： 
+
+ 输入: n = 9
+输出: 45
+ 
+
+ 
+
+ 限制： 
+
+ 
+ 1 <= n <= 10000 
+ 
+ Related Topics 位运算 递归 脑筋急转弯 👍 505 👎 0
+
+"""
+
+
+# leetcode submit region begin(Prohibit modification and deletion)
+class Solution:
+    def sumNums(self, n: int) -> int:
+        return sum([i for i in range(n + 1)])
+# leetcode submit region end(Prohibit modification and deletion)
