d9a45396c324685302ab27fc832c1b78c7f3fbd8
thomas-li-sjtu <980226547@qq.com>
2022-06-26 16:04:18 +0800

    1035：最长公共子序列，同1143；091：二维dp

--- leetcode/editor/cn/[1035]不相交的线.py
+++ leetcode/editor/cn/[1035]不相交的线.py
@@ -54,8 +54,21 @@
  Related Topics 数组 动态规划 👍 326 👎 0
 
 """
+from typing import List
 
 # leetcode submit region begin(Prohibit modification and deletion)
 class Solution:
     def maxUncrossedLines(self, nums1: List[int], nums2: List[int]) -> int:
+        dp = [[0 for _ in range(len(nums2)+1)] for _ in range(len(nums1)+1)]
+
+        for i in range(1, len(nums1)+1):
+            for j in range(1, len(nums2) + 1):
+                if nums1[i-1] == nums2[j-1]:
+                    dp[i][j] = dp[i-1][j-1] + 1
+                else:
+                    dp[i][j] = max(dp[i][j-1], dp[i-1][j-1], dp[i-1][j])
+
+        return dp[-1][-1]
+
+
 # leetcode submit region end(Prohibit modification and deletion)
